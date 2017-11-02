ad21345c42753493dc63630ceda238a2c4c6944b
stephenhartman <stephen.c.hartman@gmail.com>
2017-11-01 23:48:01 -0400

    Fix the excessive looping

--- src/project3_client/Client.java
+++ src/project3_client/Client.java
@@ -7,17 +7,17 @@ public class Client {
     private static String HOST;
     private static int PORT;
     private static int NUM_CLIENTS;
-    private static PrintWriter outputCSV;
+    private static FileWriter outputCSV;
     private Client(int num_clients) {
         this.NUM_CLIENTS = num_clients;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         HOST = args[0];
         PORT = Integer.parseInt(args[1]);
         NUM_CLIENTS = Integer.parseInt(args[2]);
-        outputCSV = new PrintWriter("output.csv");
-        outputCSV.print("Number of Clients,Total turn-around time,Average turn-around time\n");
+        outputCSV = new FileWriter("output.csv", true);
+        outputCSV.write("Number of Clients,Total turn-around time,Average turn-around time\n");
         new Client(NUM_CLIENTS).execute(HOST, PORT);
     }
 
@@ -27,29 +27,29 @@ public class Client {
         Threaded_Client[] clients = new Threaded_Client[NUM_CLIENTS];
 
         try {
+            System.out.println("Client Connected to Server " + HOST + " on Port: " + PORT);
+            System.out.println("Transmitting " + NUM_CLIENTS + " Client Requests..." );
+
             for (int x = 0; x < NUM_CLIENTS; x++) {
                 (clients[x] = new Threaded_Client(host, port, x)).start();
                 Thread.sleep(10);
             }
-            while (clients[0].getNumFinished() < NUM_CLIENTS) {
-                System.out.println("Client Connected to Server " + HOST + " on Port: " + PORT);
-                System.out.println("Transmitting " + NUM_CLIENTS + " Client Requests..." );
+            while (clients[0].getNumFinished() < NUM_CLIENTS);
 
-                for (Threaded_Client client : clients) {
-                    System.out.println(client.outputFromServer + " in " + client.totalTime + " ms.");
-                    overallTime += client.totalTime;
-                }
 
-                outputCSV.print(NUM_CLIENTS + ",");
-                System.out.println("Total Turn-around Time: " + overallTime);
-                outputCSV.print(overallTime + ",");
-                System.out.println("Average Turn-around Time: " + overallTime / NUM_CLIENTS);
-                outputCSV.print(overallTime / NUM_CLIENTS + "\n");
-
+            for (Threaded_Client client : clients) {
+                System.out.println(client.outputFromServer + " in " + client.totalTime + " ms.");
+                overallTime += client.totalTime;
             }
+
+            outputCSV.write(NUM_CLIENTS + ",");
+            System.out.println("Total Turn-around Time: " + overallTime + " ms.");
+            outputCSV.write(overallTime + ",");
+            System.out.println("Average Turn-around Time: " + overallTime / NUM_CLIENTS + " ms.");
+            outputCSV.write(overallTime / NUM_CLIENTS + "\n");
         }
-        catch (InterruptedException ie) {
-            ie.printStackTrace();
+        catch (InterruptedException | IOException e) {
+            e.printStackTrace();
             System.exit(1);
         }
     }
